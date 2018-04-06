67641a3a6abe9177021129c73524b2670813fed0
Ajay Kumar <Ajay8704276@gmail.com>
2018-04-06 18:27:01 +0530

    Bourbon base library added

---
+++ bourbonbaselib/src/main/java/com/app/bourbon/clone/bourbonbaselib/injection/PerActivity.java
@@ -0,0 +1,8 @@
+package com.app.bourbon.clone.bourbonbaselib.injection;
+
+/**
+ * Created by admin on 06/04/18.
+ */
+
+public class PerActivity {
+}
---
+++ bourbonbaselib/src/main/java/com/app/bourbon/clone/bourbonbaselib/injection/component/ApplicationComponent.java
@@ -0,0 +1,8 @@
+package com.app.bourbon.clone.bourbonbaselib.injection.component;
+
+/**
+ * Created by admin on 06/04/18.
+ */
+
+public class ApplicationComponent {
+}
